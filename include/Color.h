652f2901b4bd1c469533c605a469f7748f9a9797
BachiBachBach <thehessboi@gmail.com>
2024-08-05 13:26:40 -0500

    Color class created.

---
+++ include/Color.h
@@ -0,0 +1,20 @@
+//
+// Created by thehe on 8/5/2024.
+//
+
+#ifndef LAB_PROBLEM1_COLOR_H
+#define LAB_PROBLEM1_COLOR_H
+
+class Color
+{
+private:
+    int red;
+    int green;
+    int blue;
+
+
+public:
+    Color(int red, int green, int blue);
+};
+
+#endif //LAB_PROBLEM1_COLOR_H
---
+++ src/Color.cpp
@@ -0,0 +1,15 @@
+//
+// Created by thehe on 8/5/2024.
+//
+
+#include "../include/Color.h"
+
+Color::Color(int red, int green, int blue) {
+    this->red = red;
+    this->green = green;
+    this->blue = blue;
+}
+
+Color::RED =
+
+
